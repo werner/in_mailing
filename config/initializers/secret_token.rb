a170d47614772094bc01061d6f39ffb9bc65c4f5
werner <werner@yahoo.es>
2013-03-07 14:43:14 -0430

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inmailing::Application.config.secret_key_base = 'ae355e3f45e9e10e7c0994b4bda61b9bbce92fd33df3e2a0e9cb1fd15ee9d6c016ee321ac5840f71535d5a41fb4c12c513de744a3d45fa9a52e931208d62e7ff'
