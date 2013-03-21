b992003c35e2aeabd72b7cc1f1fac22a4c06e019
Werner Echezuria <werner@localhost.localdomain>
2013-03-21 13:53:07 -0430

    add delegates to type.name from mailing

--- app/models/mailing.rb
+++ app/models/mailing.rb
@@ -1,5 +1,6 @@
 class Mailing < ActiveRecord::Base
   belongs_to :type
+  delegate :name, to: :type, prefix: true
 
   validates_presence_of :number, :message => "Insert a number"
 
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,8 @@
 class Type < ActiveRecord::Base
+
+  has_many :mailings
+
   validates_presence_of :name
   validates_uniqueness_of :name
+
 end
