4fbed4d464dc13b7a88609c37f2148b194337fc0
Werner Echezuria <werner@localhost.localdomain>
2013-03-23 11:32:24 -0430

    validates presence of name and lastname of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates_presence_of :name
+  validates_presence_of :lastname
+
   belongs_to :department
   delegate :name, to: :department, prefix: true, :allow_nil => true
 
