14dbb7b1cb766d9405dd36d20ad36a83deac389b
Werner Echezuria <werner@localhost.localdomain>
2013-03-24 19:45:05 -0430

    a few more testing on number creation

--- spec/models/mailing_spec.rb
+++ spec/models/mailing_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
 describe Mailing do
-  it "should make unique and automatically documents numbers" do
+  def make_pair_docs(number)
     document1 = FactoryGirl.create(:mailing)
     document2 = FactoryGirl.create(:mailing)
-    document2.number.should eq("00002")
+    document2.number.should eq(number)
+  end
+  it "should make unique and automatically documents numbers" do
+    make_pair_docs("00002")
+    make_pair_docs("00004")
+    make_pair_docs("00006")
+    make_pair_docs("00008")
+    make_pair_docs("00010")
+    make_pair_docs("00012")
+    make_pair_docs("00014")
+    make_pair_docs("00016")
+    make_pair_docs("00018")
+    make_pair_docs("00020")
+    make_pair_docs("00022")
+    make_pair_docs("00024")
+    make_pair_docs("00026")
+    make_pair_docs("00028")
   end
 end
