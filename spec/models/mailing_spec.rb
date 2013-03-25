2e0c65e7aef980cea5529d10717bca1bf4c26f49
Werner Echezuria <werner@localhost.localdomain>
2013-03-25 09:54:33 -0430

    check if Mailing model is generating the number right

--- spec/models/mailing_spec.rb
+++ spec/models/mailing_spec.rb
@@ -1,25 +1,18 @@
 require 'spec_helper'
 
 describe Mailing do
-  def make_pair_docs(number)
+  def make_pair_docs(first_number, second_number)
     document1 = FactoryGirl.create(:mailing)
+    document1.number = first_number
+    document1.save
     document2 = FactoryGirl.create(:mailing)
-    document2.number.should eq(number)
+    document2.number.should eq(second_number)
   end
+
   it "should make unique and automatically documents numbers" do
-    make_pair_docs("00002")
-    make_pair_docs("00004")
-    make_pair_docs("00006")
-    make_pair_docs("00008")
-    make_pair_docs("00010")
-    make_pair_docs("00012")
-    make_pair_docs("00014")
-    make_pair_docs("00016")
-    make_pair_docs("00018")
-    make_pair_docs("00020")
-    make_pair_docs("00022")
-    make_pair_docs("00024")
-    make_pair_docs("00026")
-    make_pair_docs("00028")
+    [["00002", "00003"], ["00096", "00097"], ["00510", "00511"], ["8882312421",
+      "8882312422"], ["01318", "01319"], ["10022", "10023"], ["11126", "11127"]]. each do |number|
+        make_pair_docs number[0], number[1]
+    end
   end
 end
